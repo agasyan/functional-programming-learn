8e1e2a5bd404ab22ee5b077ea052c3cb0e769020
Agas Yanpratama <agas.yanpratama@gmail.com>
2019-12-16 14:28:48 +0700

    add referential transparrent js

--- latihan-uas/kuis/kuisBUAS.hs
+++ latihan-uas/kuis/kuisBUAS.hs
@@ -29,7 +29,52 @@ monthlyPercentagePFS = div100 . div12
 "Point-free style — aims to reduce some of the visual clutter by removing unnecessary parameter-argument mapping." 
 -}
 
--- Soal 7
+-- Soal 7 referential transparency
+
+{-
+    An expression is said to be referentially transparent
+    when it can be replaced with its corresponding value
+    without changing the application's behavior. 
+
+    Contoh:
+
+    ###yang iya
+
+    val volCube = (rusuk:Int) => math.pow(rusuk,3)
+    > volCube(3) + volCube(4) 
+    => Bisa didefiniskan satu - satu
+    volCube(3) = math.Pow(3,3)
+
+    ### yang tidak 
+    var total = 0
+
+    def addToTotal(x:Int): Int = {
+        total += x
+        return total
+    }
+    >assert (addToTotal(10)==addToTotal(10)) ---> False
+
+
+    Catatan:
+
+    # Partial
+
+    function add(a:number, b?:number){
+        if(b!==undefined){
+            return a+b;
+        } else {
+            return (b2:number) => {
+                return a+b2;
+            };
+        }
+    }
+
+    > add10 = add(10) as (b:number)
+    > let res = add10(5)
+
+
+
+-}
 
 -- Soal 8 useReducer Cimplete
 
